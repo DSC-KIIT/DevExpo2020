0e74db6e01f9e06e4ac76f4f7ade0e40ce2b7c5a
Junaid Rahim <junaidrahim8d@gmail.com>
2020-03-05 00:23:07 +0530

    completed landing page with mobile compatibility

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 //import Image from "../components/image"
-import Header from '../components/Home/Header/header'
 import SEO from '../components/seo'
+import Header from '../components/Home/Header/header'
+import Landing from '../components/Home/Landing/landing'
 
 import './css/home.css'
 
@@ -10,8 +11,7 @@ const IndexPage = () => (
   <div>
     <SEO title="DevExpo 2.0"></SEO>
     <Header></Header>
-    <br></br>
-    <h1>Hello World</h1>
+    <Landing></Landing>
   </div>
 )
 
