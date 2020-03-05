fa93a0fe988add569190d5a4c5f20310df9a1d05
Junaid Rahim <junaidrahim8d@gmail.com>
2020-03-06 02:19:12 +0530

    completed agenda for 14 march - desktop only

---
+++ src/components/Agenda/agendaHead.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './agendaHead.css'
+
+const AgendaHead = () => {
+    return (
+        <div className="agenda-head-container">
+            <h1>Agenda</h1>
+            <h6>Strap in. Hang tight. Houston, we're ready for takeoff 🚀</h6>
+            <br></br>
+        </div>
+    )
+}
+
+export default AgendaHead
