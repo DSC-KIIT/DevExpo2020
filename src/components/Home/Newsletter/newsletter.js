e3cdf3f0a6f9487b1bf0f7c0df6dafd74e5cc0d3
Junaid Rahim <junaidrahim8d@gmail.com>
2020-03-06 01:03:40 +0530

    added mobile compatibility to newsletter

--- src/components/Home/Newsletter/newsletter.js
+++ src/components/Home/Newsletter/newsletter.js
@@ -12,9 +12,12 @@ const newsletter = () => {
       <form onSubmit={submit}>
         <h1>Subscribe to our newsletter to keep <br></br> yourself updated about events</h1>
         <input className="email-input" type="email" placeholder="Enter your email address"></input>
+        <br></br>
+        <br></br>
+        <br></br>
         <input type="submit"></input>
       </form>
-        
+
     </div>
   )
 }
