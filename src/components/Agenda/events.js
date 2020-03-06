09b708674119dcdb8416724764e33ada02c86509
Junaid Rahim <junaidrahim8d@gmail.com>
2020-03-06 14:42:39 +0530

    completed agenda

--- src/components/Agenda/events.js
+++ src/components/Agenda/events.js
@@ -50,6 +50,116 @@ const Events = () => {
                 <p>Hey, we cannot tell you everything, but sure there is more stuff to be excited about.</p>
             </div>
         </div>
+
+        <div className="events-header">
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>15th March, 2020</h1>
+        </div>
+
+        <div className="events-details-container">
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Saranjit Singh, Abhishek Ray, Prachet Bhuyan</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>From KiiT Training and Placement Dept. </p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Keynote by Dean of Training and Placement committee KIIT DU. <br></br><br></br>
+                    To talk about the changing industry scenario and how it will influence engineers and jobs.
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Prajnyasis Biswal</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>Program Manager Skillenza</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Introduction to DevRel and its impact. <br></br><br></br>
+                    How communities are revolutionizing the tech industry.
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Chirag Mishra</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>SDE II at Microsoft India</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Introduction to Cloud and AI, the azure way <br></br><br></br>
+                    Will talk about Cloud Architecture, how Azure is revolutionizing the industry
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Chetan KVS</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>Product Designer at Unacademy</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Rise of the planet of designers. <br></br><br></br>
+                    Design storytelling and overview of UI and UX and sort of fun insights across design.
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Chetan KVS</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>Product Designer at Unacademy</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Rise of the planet of designers. <br></br><br></br>
+                    Design storytelling and overview of UI and UX and sort of fun insights across design.
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Sabyasachi Mukhopadhyay</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>GDE AI/ML</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Fundamental and grass-root level AI. <br></br><br></br>
+                    Will talk a bit of intermediate level but won't go though many details.
+                </p>
+            </div>
+
+            <div className="event-name">
+                <p style={{marginBottom: '1rem', textAlign:'left'}}>Aparjit Kar</p>
+                <div style={{margin: '0rem'}} className="event-description">
+                    <p style={{marginTop: '1rem', textAlign:'left'}}>Designing for mobility at Gojek</p>
+                </div>
+            </div>
+
+            <div className="event-description">
+                <p>
+                    Designing for mobility. <br></br><br></br>
+                    UX for Gojek, Design Storytelling.
+                </p>
+            </div>
+
+
+        </div>
     </div>
   )
 }
